a4a5afd5d7814948fbb8f809db91328f4e81c9a0
keralaboy <85830575+keralaboy123@users.noreply.github.com>
2024-05-11 07:12:11 -0700

    added scroll event handler

--- test.py
+++ test.py
@@ -12,36 +12,43 @@ author : github.com/keralaboy123
 
 class DragSelectTreeView(ttk.Treeview):
     "inheritable treeview customized for multiple drag selection"
-    def __init__(self,root, **kw):
-        super().__init__(root, **kw )
+
+    def __init__(self, root, **kw):
+        super().__init__(root, **kw)
         self.items = []
         self.keyholdding = False
+
         super().bind("<Button-1>", self.start_selection)
         super().bind("<B1-Motion>", self.update_selection)
         super().bind("<ButtonRelease-1>", self.end_selection)
-        self.bind("<MouseWheel>", self.update_selection)
+        super().bind("<MouseWheel>", self.update_selection)
 
-    def getselection_list(self):
-        return self.items
-
-    def _press(self,event):
-        pass
-    def _release(self,event):
-        pass
-    def _motion (self,event):
+    def _scroll(self,event):
         pass
 
+    def _press(self, event):
+        pass
 
-    def bind(self,event_string,function):
+    def _release(self, event):
+        pass
+
+    def _motion(self, event):
+        pass
+
+    def bind(self, event_string, function):
         if event_string == "<Button-1>":
             self._press = function
         elif event_string == "<B1-Motion>":
             self._motion = function
         elif event_string == "<ButtonRelease-1>":
             self._release = function
+        elif event_string == "<MouseWheel>":
+            self._scroll = function
         else:
-            super().bind(event_string,function)
+            super().bind(event_string, function)
 
+    def getselection_list(self):
+        return self.items
 
     def start_selection(self, event):
         self.items = []
@@ -55,11 +62,11 @@ class DragSelectTreeView(ttk.Treeview):
 
     def update_selection(self, event):
         if self.keyholdding:
-            
-           item = self.identify('item', event.x, event.y)
-           if not item in self.items:
-              self.items.append(item)
-              
+
+            item = self.identify('item', event.x, event.y)
+            if not item in self.items:
+                self.items.append(item)
+
         self.show_multiple_selection()
         self._motion(event)
 
@@ -69,19 +76,23 @@ class DragSelectTreeView(ttk.Treeview):
 
 
 if __name__ == "__main__":
-        root = tk.Tk()
-        tree = DragSelectTreeView(root, columns=("Name", "Age", "Country"))
-        tree.configure(selectmode="extended")
-        style = ttk.Style()
-        style.map("Treeview", background=[('selected', 'orange')])
+    root = tk.Tk()
+    style = ttk.Style()
+    style.map("Treeview", background=[('selected', 'orange')])
 
-        for i in range(110):
-             tree.insert("", "end", text=f"orange {i}",values=("John Doe", "30", "USA"))
+    tree = DragSelectTreeView(root, columns=("Name", "Age", "Country"))
+    tree.configure(selectmode="extended")
 
-        tree.pack(side ='left')
-        tree.bind("<ButtonRelease-1>", lambda x: print("selected items are = ",tree.selection()))
-        scroll = ttk.Scrollbar(root, orient="vertical", command=tree.yview)
-        scroll.pack(side='right', fill='y')
-        tree.configure(yscrollcommand=scroll.set)
 
-        root.mainloop()
+    for i in range(110):
+        item = tree.insert("", "end", text=f"orange {i}", values=("John Doe", "30", "USA"))
+        tree.insert(item, "end", text=f"orange {i}", values=("John Doe", "30", "USA"))
+
+
+    tree.pack(expand=True,side='left')
+    tree.bind("<ButtonRelease-1>", lambda x: print("selected items are = ", tree.selection()))
+
+    scroll = ttk.Scrollbar(root, orient="vertical", command=tree.yview)
+    scroll.pack(side='right', fill='y')
+    tree.configure(yscrollcommand=scroll.set)
+    root.mainloop()
