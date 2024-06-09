fbd75cfba29da60e8c95a1c00011942bc66f5699
keralaboy <85830575+keralaboy123@users.noreply.github.com>
2024-06-09 02:37:03 -0700

    Update googlescrapper.py output folder varaible refactored

--- googlescrapper.py
+++ googlescrapper.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import json
 import threading
 import os
+    
 
 USERAGENT = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0'}
 
@@ -75,16 +76,16 @@ class linkscrapper(googlescrapper):
         html = one_website.readhtml()
         return html
 
-    def scrap_all(self,urldict,output_file=""):
+    def scrap_all(self, urldict, output_folder =""):
         for one in urldict:
             link = one["link"]
             html_of_target_link = self.get_target_site_data(link)
             dict_data = {"data" : html_of_target_link}
             dict_data.update(one)
-            self.save(dict_data,folder = output_file)
+            self.save(dict_data, folder = output_folder)
 
 
-def ask_by_cli(query = "",save="",show_incli="",start="0",output_file=""):
+def ask_by_cli(query = "", save="", show_incli="", start="0", output_folder=""):
     gscrapper = linkscrapper()
     query = query or  input(" enter a search term > ")
     results = gscrapper.get_links(query,start)
@@ -92,26 +93,27 @@ def ask_by_cli(query = "",save="",show_incli="",start="0",output_file=""):
         if not  show_incli:
             print(f"{index + 1}. {result['title']}\n{result['link']}\n")
 
-    query = save or input("to save results type a folder path else press enter > ")
-    if query:
-        gscrapper.scrap_all(results,query)
+    folder = output_folder or input("to save results type a folder path else press enter > ")
+    if folder:
+        gscrapper.scrap_all(results,folder)
+        print("everything is saved to ",folder)
 
 class autoscrapper(threading.Thread):
     "this is runni9ng in thread for fast processing of scrapping"
-    def __init__(self,query,start,output_file=""):
+    def __init__(self, query, start, output_folder=""):
         super().__init__()
         self.query = query
         self.start_index = start
-        self.output_file= output_file
+        self.output_folder= output_folder
 
     def run(self) -> None:
-        ask_by_cli(self.query, save=True, show_incli=True, start =str(self.start_index),output_file=self.output_file)
+        ask_by_cli(self.query, save=True, show_incli=True, start = str(self.start_index), output_folder = self.output_folder)
 
 
-def scrap_maximum(query, max_results = 7,output_file=""):
+def scrap_maximum(query, max_results = 7, output_folder=""):
         "this is used for running in thread"
         for start in range(6,int(max_results),6):
-            scrapper= autoscrapper(query,start,output_file)
+            scrapper= autoscrapper(query, start, output_folder)
             scrapper.start()
 
 
@@ -124,7 +126,7 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--path", help = "path to folder  where files should be saved. ")
     args = parser.parse_args()
     if args.query  and args.max and args.path:
-        scrap_maximum(args.query,max_results = args.max,output_file=args.path )
+        scrap_maximum(args.query, max_results = args.max, output_folder=args.path)
     else:
         ask_by_cli()
 
